05ef8a683ce794665a40584a54e73ee8ecb5e0db
Kevin Hartman <kevin@hart.mn>
2022-04-14 14:46:36 -0400

    Add randomized test scheduled CI job for TOQM.

---
+++ .github/workflows/randomized_tests.py
@@ -0,0 +1,34 @@
+name: Randomized tests
+on:
+  push:
+  schedule:
+    - cron:  '42 3 * * *'
+  workflow_dispatch:
+jobs:
+  randomized_tests:
+    name: Randomized tests
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: actions/setup-python@v2
+        name: Install Python
+        with:
+          python-version: '3.8'
+      - name: Install dependencies
+        run: |
+          pip install -U -r requirements.txt -c constraints.txt
+          pip install -U -r requirements-dev.txt coveralls -c constraints.txt
+          pip install -c constraints.txt -e .
+          pip install "qiskit-terra"
+          pip install "qiskit-ibmq-provider" -c constraints.txt
+          pip install "qiskit-aer"
+      - name: Run randomized tests
+        run: python3 -m unittest discover -s test/randomized -t . -v
+      - name: Create comment on failed test run
+        if: ${{ failure() }}
+        uses: peter-evans/create-or-update-comment@v1
+        with:
+          issue-number: 16
+          body: |
+            Randomized tests failed at commit ${{ github.sha }}.
+            _Logs_: ${{ github.server_url }}/${{ github.repository }}/actions/runs/${{ github.run_id }}.
