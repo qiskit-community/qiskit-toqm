fbf9533173a72909f4821f887c9fe84e372fc35e
Kevin Hartman <kevin@hart.mn>
2022-10-28 17:46:31 -0400

    Fix wheels. (#33)

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import re
 import sys
 
 try:
@@ -12,10 +14,26 @@ except ImportError:
 
 from setuptools import find_packages
 
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+# Read long description from README.
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
+with open(README_PATH) as readme_file:
+    README = re.sub(
+        "<!--- long-description-skip-begin -->.*<!--- long-description-skip-end -->",
+        "",
+        readme_file.read(),
+        flags=re.S | re.M,
+    )
+
 setup(
     name="qiskit-toqm",
     version="0.1.0",
     description="Qiskit transpiler passes for the TOQM algorithm",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/qiskit-toqm/qiskit-toqm",
     author="Qiskit Development Team",
     author_email="hello@qiskit.org",
     license="Apache 2.0",
@@ -34,21 +52,22 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering",
     ],
-    keywords="qiskit sdk quantum",
+    keywords="qiskit sdk quantum toqm",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     cmake_install_dir="src/qiskit_toqm/native",
+    install_requires=REQUIREMENTS,
     include_package_data=True,
     extras_require={"test": ["pytest"]},
     python_requires=">=3.7",
     project_urls={
-        "Bug Tracker": "https://github.com/kevinhartman/qiskit-toqm/issues",
-        "Documentation": "https://github.com/kevinhartman/qiskit-toqm",
-        "Source Code": "https://github.com/kevinhartman/qiskit-toqm",
+        "Bug Tracker": "https://github.com/qiskit-toqm/qiskit-toqm/issues",
+        "Documentation": "https://github.com/qiskit-toqm/qiskit-toqm",
+        "Source Code": "https://github.com/qiskit-toqm/qiskit-toqm",
     },
-    entry_points = {
+    entry_points={
         'qiskit.transpiler.routing': [
             'toqm = qiskit_toqm.toqm_plugin:ToqmSwapPlugin',
-        ]
+        ],
     },
 )
